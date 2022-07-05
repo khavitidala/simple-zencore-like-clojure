f698bae3ebaaa2da7547e3fa9fec26bfb5879666
Ryan Abdurohman <ryanabdurohman81@gmail.com>
2022-07-05 10:01:43 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# ZenClone
+
+A simple zencore-like app
+
+## Prerequisites
+
+You will need [Leiningen][] 2.0.0 or above installed.
+
+[leiningen]: https://github.com/technomancy/leiningen
+
+## Running
+
+To start a web server for the application, run:
+
+    lein ring server
+
+## Released under MIT License
+
+Copyright © 2022 Ryan Abdurohman.
---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject zencore "1.0.0"
+  :description "A simple zencore-like app"
+  :url "https://github.com/khavitidala/simple-zencore-like-clojure"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler zencore.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.2"]]}})
